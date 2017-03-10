131fccc6a2bbf7cc9fa33656ef1deee3bb26b33e
zhucz <375683050@qq.com>
2017-03-10 18:38:55 +0800

    update

--- src/com/zeng/ua/activity/MainActivity.java
+++ src/com/zeng/ua/activity/MainActivity.java
@@ -3,16 +3,62 @@ package com.zeng.ua.activity;
 import com.zeng.ua.app.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class MainActivity extends Activity {
-
+	private static final String TAG = "MainActivity";
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Log.i(TAG, "onCreate");
+    }
+    
+    @Override
+    protected void onStart() {
+    	// TODO Auto-generated method stub
+    	super.onStart();
+    }
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
+    }
+    
+    @Override
+    protected void onRestart() {
+    	// TODO Auto-generated method stub
+    	super.onRestart();
+    }
+    
+    @Override
+    protected void onPause() {
+    	// TODO Auto-generated method stub
+    	super.onPause();
+    }
+    
+    @Override
+    protected void onStop() {
+    	// TODO Auto-generated method stub
+    	super.onStop();
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	// TODO Auto-generated method stub
+    	super.onDestroy();
+    }
+    
+    @Override
+    protected void onNewIntent(Intent intent) {
+    	// TODO Auto-generated method stub
+    	super.onNewIntent(intent);
     }
 
 
--- src/com/zeng/ua/base/MyApplication.java
+++ src/com/zeng/ua/base/MyApplication.java
@@ -1,11 +1,52 @@
 package com.zeng.ua.base;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
 
 public class MyApplication extends Application {
-
+	private static final String TAG = "MyApplication";
+	
+	
 	public MyApplication() {
 		// TODO Auto-generated constructor stub
+		Log.i(TAG, "constructor");
 	}
+	
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		Log.i(TAG, "onCreate");
+		super.onCreate();
+	}
+	
+	@Override
+	public void onTerminate() {
+		// TODO Auto-generated method stub
+		Log.i(TAG, "onTerminate");
+		super.onTerminate();
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// TODO Auto-generated method stub
+		Log.i(TAG, "onConfigurationChanged");
+		super.onConfigurationChanged(newConfig);
+	}
+	
+	@Override
+	public void onLowMemory() {
+		// TODO Auto-generated method stub
+		Log.i(TAG, "onLowMemory");
+		super.onLowMemory();
+	}
+	
+	@Override
+	public void onTrimMemory(int level) {
+		// TODO Auto-generated method stub
+		Log.i(TAG, "onTrimMemory");
+		super.onTrimMemory(level);
+	}
+	
 
 }
